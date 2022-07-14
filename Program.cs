ca45c1b536c38028d0aaf2c120d12b2e170f96e5
Rimabdurahman <108622986+Rimabdurahman@users.noreply.github.com>
2022-07-14 15:02:37 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,45 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace helloworld
+namespace gradingsystem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+           
+            int x=int.Parse(Console.ReadLine());
             
+           
+            if (x >= 90)
+            {
+                Console.WriteLine("Result is A");
+            }
+            else if ( x >= 70){
+                Console.WriteLine("Result is B");
+            }
+            else if(x >= 60){
+                Console.WriteLine("Result is C");
+            }
+            else if(x >= 50){
+                Console.WriteLine("Result is D");
+            }
+            else if (x >100)
+            {
+                Console.WriteLine("Error :Grade should be less than hundred");
+            }
+            else if (x <0)
+            {
+                Console.WriteLine("Error :Grade should be greater than zero");
+            }
 
-            Console.WriteLine("hello world");
+            else
+            {
+                Console.WriteLine("F");
+            }
         }
     }
 }
